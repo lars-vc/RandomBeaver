bcf48b2b2163573f0033da24589f3d47bbd52dc0
Lars Van Cauter <larsvancauter@gmail.com>
2021-01-13 18:20:24 +0100

    should be executable anywhere now

--- code/main.py
+++ code/main.py
@@ -5,9 +5,13 @@ import random
 import pathlib
 from PIL import Image
 
+def parentPath():
+    p = pathlib.Path(pathlib.Path().absolute())
+    return str(p.parent.absolute()).replace("\\", "/")
+
 
 def maxImageNum():
-    onlyfiles = [f for f in listdir("../images/") if isfile(join("../images/", f))]
+    onlyfiles = [f for f in listdir(parentPath() + "/images") if isfile(join(parentPath() + "/images", f))]
     for i, x in enumerate(onlyfiles):
         onlyfiles[i] = int(re.search(r"[0-9]+", x).group(0))
     return max(onlyfiles)
@@ -16,12 +20,14 @@ def maxImageNum():
 def getRandomUrl():
     x = random.randint(1, maxImageNum())
     s = f"/images/beaverpicture ({x}).jpg"
-    p = pathlib.Path(pathlib.Path().absolute())
-    return str(p.parent.absolute()).replace("\\", "/") + s
+    return parentPath() + s
+
+
 
 
 def showImage(url):
     im = Image.open(url)
     im.show()
+
 
 showImage(getRandomUrl())
